8e9c1ce2d33c688369551bf720b306a463cd5f5b
ramraj24019 <81388110+ramraj24019@users.noreply.github.com>
2025-08-01 13:21:03 +0530

    Update App.js
    Show ChipShop component

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import ChipShop from "./src/components/ChipShop";
 import LoginScreen from "./src/components/LoginScreen";
-import "./src/index.css"; // अगर Tailwind इस्तेमाल हो रहा है
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <LoginScreen />
+      <ChipShop />
+      {/* <LoginScreen /> */}
     </div>
   );
 }
