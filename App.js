0c86ed7d39b4b1193b879c779dbd3fc168d841d1
ramraj24019 <81388110+ramraj24019@users.noreply.github.com>
2025-08-01 15:05:13 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ChipShop from "./src/components/ChipShop";
 import LoginScreen from "./src/components/LoginScreen";
+import ChipShop from "./src/components/ChipShop";
 import GameScreen from "./src/components/GameScreen";
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-        <Routes>
-          <Route path="/" element={<LoginScreen />} />
-          <Route path="/shop" element={<ChipShop />} />
-          <Route path="/game" element={<GameScreen />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<LoginScreen />} />
+        <Route path="/chipshop" element={<ChipShop />} />
+        <Route path="/game" element={<GameScreen />} />
+      </Routes>
     </Router>
   );
 }
