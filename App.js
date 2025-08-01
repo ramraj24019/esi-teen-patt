a6ae05fb8240d021f22b53e076107f8997a77fcc
ramraj24019 <81388110+ramraj24019@users.noreply.github.com>
2025-08-01 14:59:30 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import ChipShop from "./src/components/ChipShop";
 import LoginScreen from "./src/components/LoginScreen";
+import AdminPanel from "./src/screens/AdminPanel";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <ChipShop />
-      {/* <LoginScreen /> */}
-    </div>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LoginScreen">
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="ChipShop" component={ChipShop} />
+        <Stack.Screen name="AdminPanel" component={AdminPanel} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
